87d9cf5df85acece13d3390a86e9548a4217febf
abboswick <45312168+abboswick@users.noreply.github.com>
2022-11-18 21:46:42 +0500

    Add files via upload

--- src/server.js
+++ src/server.js
@@ -1,6 +1,65 @@
 const express = require("express");
 const app = express();
+PORT=4000;
+const fs = require("fs");
+const path= require("path")
+let users=fs.readFileSync("./database/user.json", "utf-8").trim() || [];
+users = JSON.parse(users);
 
-PORT = 4000;
+app.use(express.json());
+
+
+app.post("/login", (req,res) => {
+    
+    console.log(req.body);
+    req.body.id=users.length>0 ? users[users.length-1].id +1 : 1;
+    const user_email=req.body.email;
+    const user_password=req.body.password;
+    const db_password=getPassword(user_email);
+
+    if(user_password==db_password && db_password != null){
+        res.status(200).json({
+            message:"Login successfull",
+            status:200
+        })
+    }else{
+        res.status(403).json({
+            message:"Login unsuccessfull",
+            status:403
+        });
+     }
+    });
+
+function getPassword(user_email) {
+   for (let index = 0; index < users.length; index++) {
+    const user = users[index]; 
+    const email = user.email;
+    const password = user.password;
+
+    if (user_email == email) {
+        return password;
+    }
+}   
+}
+
+
+app.post("/signup", (req,res) => {
+    
+    
+    req.body.id=users.length>0 ? users[users.length-1].id +1 : 1;
+    console.log(typeof users);
+    users.push(req.body);
+
+
+
+    fs.writeFileSync("./database/user.json", JSON.stringify(users,null,2));    
+    
+    res.status(201).json({
+    message:"new user created",
+    status:201
+})
+});
+
+
 
 app.listen(PORT, ()=>console.log("Server is running on http://localhost:"+PORT))
---
+++ view/main.js
@@ -0,0 +1,64 @@
+function setFormMessage(formElement, type, message) {
+    const messageElement = formElement.querySelector(".form__message");
+
+    messageElement.textContent = message;
+    messageElement.classList.remove("form__message--success", "form__message--error");
+    messageElement.classList.add(`form__message--${type}`);
+}
+
+function setInputError(inputElement, message) {
+    inputElement.classList.add("form__input--error");
+    inputElement.parentElement.querySelector(".form__input-error-message").textContent = message;
+}
+
+function clearInputError(inputElement) {
+    inputElement.classList.remove("form__input--error");
+    inputElement.parentElement.querySelector(".form__input-error-message").textContent = "";
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const loginForm = document.querySelector("#login");
+    const createAccountForm = document.querySelector("#createAccount");
+
+    document.querySelector("#linkCreateAccount").addEventListener("click", e => {
+        e.preventDefault();
+        loginForm.classList.add("form--hidden");
+        createAccountForm.classList.remove("form--hidden");
+    });
+
+    document.querySelector("#linkLogin").addEventListener("click", e => {
+        e.preventDefault();
+        loginForm.classList.remove("form--hidden");
+        createAccountForm.classList.add("form--hidden");
+    });
+
+    loginForm.addEventListener("submit", e => {
+        e.preventDefault();
+
+        // Perform your AJAX/Fetch login
+
+        setFormMessage(loginForm, "error", "Invalid username/password combination");
+    });
+
+    document.querySelectorAll(".form__input").forEach(inputElement => {
+        inputElement.addEventListener("blur", e => {
+            if (e.target.id === "signupUsername" && e.target.value.length > 0 && e.target.value.length < 10) {
+                setInputError(inputElement, "Username must be at least 10 characters in length");
+            }
+        });
+
+        inputElement.addEventListener("input", e => {
+            clearInputError(inputElement);
+        });
+    });
+});
+function SignUp(params) {
+    var request = new XMLHttpRequest(); // Create a request variable and assign a new XMLHttpRequest object to it.
+    request.open('POST', 'localhost:4000/signup'); // Open a new connection, using the GET request on the URL endpoint
+    request.send(JSON.stringify(params));
+    
+    request.onload = async function () {
+        var data = JSON.parse(this.response);
+        document.getElementById('testId').InnerHTML = data // depending on your response targert your desired property.
+    }
+}
